3a9bd951c8f320c67a88858c92b6a8f8e60732e4
Adam Crenshaw <crensma11@gmail.com>
2017-11-27 21:44:43 -0600

    Added Database to Flask app

--- application.py
+++ application.py
@@ -1,11 +1,27 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Sport, GearItem
 app = Flask(__name__)
+
+
+engine = create_engine('sqlite:///sportgear.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 
 @app.route('/')
 @app.route('/hello')
 def HelloWorld():
-    return "Hello World"
+    sport = session.query(Sport).first()
+    items = session.query(GearItem).filter_by(sport_id=sport.id)
+    output = ''
+    for i in items:
+        output += i.name
+        output += '</br>'
+    return output
 
 
 if __name__ == '__main__':
