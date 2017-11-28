c14582f547cf187e5520c3549892b438255b59e4
Adam Crenshaw <crensma11@gmail.com>
2017-11-27 22:17:51 -0600

    created routes to create, edit and delete gear items

--- application.py
+++ application.py
@@ -26,11 +26,24 @@ def sportGear(sport_id):
         output += '</br>'
     return output
 
+
 # Create route for newGearItem function
+@app.route('/sport/<int:sport_id>/new/')
+def newGearItem(sport_id):
+    return "page to create a new gear item. Task 1 complete!"
+
 
 # Create route for editGearItem function
+@app.route('/sport/<int:sport_id>/<int:gear_id>/edit/')
+def editGearItem(sport_id, gear_id):
+    return "page to edit a gear item. Task 2 complete!"
+
 
 # Create route for deleteGearItem function
+@app.route('/sport/<int:sport_id>/<int:gear_id>/delete/')
+def deleteGearItem(sport_id, gear_id):
+    return "page to delete a gear item. Task 3 complete!"
+
 
 if __name__ == '__main__':
     app.debug = True
