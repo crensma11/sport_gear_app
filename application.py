4ffa8ccca0ed1310877d8aa9c8d17ed1e5e1af06
Adam Crenshaw <crensma11@gmail.com>
2017-11-28 22:18:06 -0600

    created new gear item template with requests and redirects

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Sport, GearItem
@@ -21,9 +21,15 @@ def sportGear(sport_id):
 
 
 # Create route for newGearItem function
-@app.route('/sport/<int:sport_id>/new/')
+@app.route('/sport/<int:sport_id>/new/', methods=['GET', 'POST'])
 def newGearItem(sport_id):
-    return "page to create a new gear item. Task 1 complete!"
+    if request.method == 'POST':
+        newItem = GearItem(name=request.form['name'], sport_id=sport_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('sportGear', sport_id=sport_id))
+    else:
+        return render_template('newgearitem.html', sport_id=sport_id)
 
 
 # Create route for editGearItem function
